18f86bb0bd7cfa0371070cfa439f36ff0fea300f
amitay.s <amitay.sich@gmail.com>
2025-08-20 09:00:13 +0300

    add validation set

--- bioemb/downstream_eval.py
+++ bioemb/downstream_eval.py
@@ -23,10 +23,13 @@ class PredictionHead(nn.Module):
     def __init__(self, bottleneck_dim: int, output_dim: int = 1):
         super().__init__()
         self.head = nn.Sequential(
-            nn.Linear(bottleneck_dim, bottleneck_dim // 2),
-            nn.ReLU(),
+            # nn.Linear(bottleneck_dim, bottleneck_dim // 2),
+            # nn.ReLU(),
+            # nn.Dropout(0.3),
+            # nn.Linear(bottleneck_dim // 2, output_dim)
             nn.Dropout(0.3),
-            nn.Linear(bottleneck_dim // 2, output_dim)
+            nn.Linear(bottleneck_dim, output_dim)
+
         )
 
     def forward(self, x):
