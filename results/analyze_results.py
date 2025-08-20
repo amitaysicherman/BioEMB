d667e58538eb2ffa22cff7aa5f367d2f5b10566f
amitay.s <amitay.sich@gmail.com>
2025-08-20 12:33:20 +0300

    analyze scores

--- results/analyze_results.py
+++ results/analyze_results.py
@@ -3,7 +3,6 @@ import pandas as pd
 from bioemb.data_manager import dataset_to_task_type
 
 
-
 def read_csv_with_bug(file_name):
     first_row_data = pd.read_csv(
         file_name,
@@ -13,33 +12,33 @@ def read_csv_with_bug(file_name):
 
     df = pd.read_csv(
         file_name,
-        header=None,     # Tell pandas the file has no header row to interpret.
-        skiprows=2,      # Skip the first line of the file.
-        names=["?"]+list(first_row_data.columns)
+        header=None,  # Tell pandas the file has no header row to interpret.
+        skiprows=2,  # Skip the first line of the file.
+        names=["?"] + list(first_row_data.columns)
     )
     return first_row_data, df
 
 
-
 all_tasks = os.listdir("results")
-for task_name in all_tasks:
-    if not os.path.isdir(os.path.join('results', task_name)):
+for dir_name in all_tasks:
+    if not os.path.isdir(os.path.join('results', dir_name)):
         continue
-    res_file = os.path.join('results', task_name, "evaluation_log.csv")
+    res_file = os.path.join('results', dir_name, "evaluation_log.csv")
     if not os.path.exists(res_file):
-        print(f"Results file for {task_name} does not exist.")
+        print(f"Results file for {dir_name} does not exist.")
         continue
     first_row, other_rows = read_csv_with_bug(res_file)
     pre_trained_score = first_row["eval_downstream_auc"].values[0]
     bioemb_scores_test = other_rows["eval_downstream_auc"]
-    bioemb_scores_valid= other_rows["eval_downstream_auc_valid"]
-
-    task_type= dataset_to_task_type[task_name]
+    bioemb_scores_valid = other_rows["eval_downstream_auc_valid"]
+    task_name = dir_name.split("~")[0]
+    task_type = dataset_to_task_type[dir_name]
     if task_type == "classification":
         best_index = bioemb_scores_valid.idxmax()
         best_score = bioemb_scores_test[best_index]
     elif task_type == "regression":
         best_index = bioemb_scores_valid.idxmin()
-        best_score= bioemb_scores_test[best_index]
+        best_score = bioemb_scores_test[best_index]
 
-    print(f"Task: {task_name}[{task_type}], Pre-trained Score: {pre_trained_score:.4f}, BioEmb Best Score: {best_score:.4f}")
+    print(
+        f"Task: {dir_name}[{task_type}], Pre-trained Score: {pre_trained_score:.4f}, BioEmb Best Score: {best_score:.4f}")
