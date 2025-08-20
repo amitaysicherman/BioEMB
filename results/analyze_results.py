b7254c12c4f275e7d6107d9d4480fa26c7ab7a65
amitay.s <amitay.sich@gmail.com>
2025-08-20 10:25:38 +0300

    analyze scores

--- results/analyze_results.py
+++ results/analyze_results.py
@@ -2,16 +2,34 @@ import os
 import pandas as pd
 from bioemb.data_manager import dataset_to_task_type
 
+
+
+def read_csv_with_bug(file_name):
+    first_row_data = pd.read_csv(
+        file_name,
+        header=0,
+        nrows=2  # Read only the first row.
+    )
+
+    df = pd.read_csv(
+        file_name,
+        header=None,     # Tell pandas the file has no header row to interpret.
+        skiprows=2,      # Skip the first line of the file.
+        names=["?"]+list(first_row_data.columns)
+    )
+    return first_row_data, df
+
+
+
 all_tasks = os.listdir("results")
 for task_name in all_tasks:
     if not os.path.isdir(os.path.join('results', task_name)):
         continue
     res_file = os.path.join('results', task_name, "evaluation_log.csv")
+    first_row, other_rows = read_csv_with_bug(res_file)
     data = pd.read_csv(res_file)
-    first_row = data.iloc[0]
-    pre_trained_score = first_row["eval_downstream_auc"]
-    other_rows = data.iloc[1:]
-    bioemb_scores = other_rows["eval_loss"]  # TODO: fix the bug in the name.
+    pre_trained_score = first_row["first_row_data"]
+    bioemb_scores = other_rows["first_row_data"]  # TODO: fix the bug in the name.
     if dataset_to_task_type[task_name] == "classification":
         best_score = bioemb_scores.max()
     elif dataset_to_task_type[task_name] == "regression":
